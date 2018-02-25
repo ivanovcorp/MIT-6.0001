457fb3ef6b52234c9b25b1d4d2b275e0866361bf
Ivan Ivanov <ivanovcorp0@gmail.com>
2018-02-25 19:30:34 +0200

    Added other lectures code.

--- L04_DecompositionAbstractionFunctions/__init__.py
+++ L04_DecompositionAbstractionFunctions/__init__.py
@@ -151,17 +151,17 @@ z = g(x)
 ## EXAMPLE: complicated scope, test yourself!
 ## Python Tutor link: http://www.pythontutor.com/visualize.html#code=def%20f(x%29%3A%0A%20%20%20x%20%3D%20x%20%2B%201%0A%20%20%20print('in%20f(x%29%3A%20x%20%3D',%20x%29%0A%20%20%20return%20x%0A%0Ax%20%3D%203%0Az%20%3D%20f(x%29%0Aprint('in%20main%20program%20scope%3A%20z%20%3D',%20z%29%0Aprint('in%20main%20program%20scope%3A%20x%20%3D',%20x%29%0A%0Adef%20g(x%29%3A%0A%20%20%20%20def%20h(x%29%3A%0A%20%20%20%20%20%20%20%20x%20%3D%20x%2B1%0A%20%20%20%20%20%20%20%20print(%22in%20h(x%29%3A%20x%20%3D%20%22,%20x%29%0A%20%20%20%20x%20%3D%20x%20%2B%201%0A%20%20%20%20print('in%20g(x%29%3A%20x%20%3D%20',%20x%29%0A%20%20%20%20h(x%29%0A%20%20%20%20return%20x%0A%0Ax%20%3D%203%0Az%20%3D%20g(x%29%0Aprint('in%20main%20program%20scope%3A%20x%20%3D%20',%20x%29%0Aprint('in%20main%20program%20scope%3A%20z%20%3D%20',%20z%29%0A&cumulative=false&curInstr=0&heapPrimitives=false&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
 #########################
-def f(x):
+def f1(x):
    x = x + 1
    print('in f(x): x =', x)
    return x
 
 x = 3
-z = f(x)
+z = f1(x)
 print('in main program scope: z =', z)
 print('in main program scope: x =', x)
 
-def g(x):
+def g1(x):
     def h(x):
         x = x+1
         print("in h(x): x = ", x)
@@ -171,6 +171,6 @@ def g(x):
     return x
 
 x = 3
-z = g(x)
+z = g1(x)
 print('in main program scope: x = ', x)
 print('in main program scope: z = ', z)
--- PythonLangExamples/__init__.py
+++ PythonLangExamples/__init__.py
@@ -59,4 +59,109 @@ print('This is the result of funcWithReturn(5,5):',funcWithReturn(5, 5))
 def funcWithoutReturn(num1, num2):
     print('This is printed from funcWithoutReturn. The result of :',num1,'+',num2, '=', (num1 + num2))
 funcWithoutReturn(6, 5) 
+
+# tuples
+te = (5, 'ivan', 2.2)
+for i in te:
+    print(i)
+print('Lenght of the tuple is:', len(te))
+
+print('Slice tuple from 1 to 2', te[1:2])
+print('Slice tuple from 1 to 3', te[0:3])
+print('Slice tuple from 0 to 2', te[0:2])
+
+# lists
+L = [2, 'a', 3, [0, 1]]
+for i in L:
+    print('Element:', i)
+print('Size of the list is:', len(L))
+print('Element L[1] before change:', L[1])
+L[1] = 'b'
+print('Element L[1] after change:', L[1])
+print('Adding element to the lis: 155')
+L.append(155)
+for i in L:
+    print('Element:', i)
+print('Removing element 155.')
+L.remove(155)
+for i in L:
+    print('Element:', i)
+L1 = [1, 2, 3]
+L2 = [4, 5, 6]
+LF = L1 + L2
+for i in LF:
+    print('Element:', i)
+print('Remove last elem and get its value:',LF.pop())
+for i in LF:
+    print('Element:', i)
+print('Printing list after removing value at [0]')
+del(LF[0])
+for i in LF:
+    print('Element:', i)
+str1 = 'Convert me to list'
+L3 = list(str1)
+for i in L3:
+    print('Element:', i)
+str2 = '5 < 6 ha ha'
+print('Before split:', str2)
+L4 = str2.split('<')
+for i in L4:
+    print('Element:', i)
+L5 = ['a', 'b', 'c']
+print('Joining L5 to string:', ''.join(L5))
+L6 = [9, 6, 0, 3]
+print('Sorted:', sorted(L6))
+print('The list is not changed after calling sorted:', L6)
+L6.sort()
+print('However when calling sort funct. the list is sorted:',L6)
+
+# aliases
+
+warm = ['red', 'yellow', 'orange']
+hot = warm
+hot.append('pink')
+print('hot:', hot)
+print('warm:', warm)
+
+# clone
+cool = ['blue', 'green', 'grey']
+chill = cool[:]
+chill.append('black')
+print('chill:', chill)
+print('cool:'   , cool)
+
+# Recursion
+def mult(a, b):
+    if b == 1:
+        return a
+    else :
+        return a + mult(a, b - 1)
     
+print(mult(6, 4))
+
+def fact(n):
+    if n == 1:
+        return 1
+    else :
+        return n * fact(n - 1)
+print(fact(10))
+
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else :
+        return fib(x - 1) + fib(x - 2)
+print(fib(4))
+
+# dictionary
+my_dict = {}
+grades = {'Ivan' : 6, 'Pesho' : 3, 'Gosho' : 2}
+print('All grades:', grades)
+print('Ivan:', grades['Ivan'])
+grades['Tosho'] = 5
+print('Added grade for Tosho:', grades)
+print('Is Ivan in the dictionary:', 'Ivan' in grades)
+del(grades['Tosho'])
+print('Deleted Tosho:', grades)
+print('All keys:', grades.keys())
+print('All keys:', grades.values())
